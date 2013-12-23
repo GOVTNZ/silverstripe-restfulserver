843dfff778a34d20fb04cfe4e6ae49de028c87c0
James Goodman <james.s.goodman@gmail.com>
2013-12-23 15:18:10 +0000

    Add ability to access API end points via aliases

--- tests/unit/APIAliasTest.php
+++ tests/unit/APIAliasTest.php
@@ -6,6 +6,19 @@ class APIAliasTest extends SapphireTest {
 		'APIAliasTestObject'
 	);
 
+	public function setUpOnce() {
+		$this->cleanAliasCache();
+	}
+
+	public function tearDownOnce() {
+		$this->cleanAliasCache();
+	}
+
+	private function cleanAliasCache() {
+		$aliasCache = SS_Cache::factory(RestfulServerV2::ALIAS_CACHE_KEY);
+		$aliasCache->clean();
+	}
+
 	public function testAPIAlias() {
 		$aliasURL = '/api/v2/testobjects';
 		$defaultURL = '/api/v2/APIAliasTestObject';
@@ -15,7 +28,17 @@ class APIAliasTest extends SapphireTest {
 
 		$this->assertEquals(200, $aliasResponse->getStatusCode(), 'Did not receive a 200 response on alias URL');
 
-		$this->assertEquals($defaultResponse, $aliasResponse, 'Alias response did not match default response');
+		$this->assertEquals($defaultResponse->getBody(), $aliasResponse->getBody(), 'Alias response did not match default response');
+	}
+
+	public function testAPIAliasCache() {
+		$aliasURL = '/api/v2/testobjects';
+
+		// grab results twice to confirm cache does not break anything
+		Director::test($aliasURL, null, null, 'GET');
+		$aliasResponse = Director::test($aliasURL, null, null, 'GET');
+
+		$this->assertEquals(200, $aliasResponse->getStatusCode(), 'Did not receive a 200 response on alias URL');
 	}
 
 }
@@ -23,7 +46,7 @@ class APIAliasTest extends SapphireTest {
 class APIAliasTestObject extends DataObject implements TestOnly {
 
 	private static $api_access = array(
-		'alias' => 'testobjects'
+		'end_point_alias' => 'testobjects'
 	);
 
 	public static $db = array(
