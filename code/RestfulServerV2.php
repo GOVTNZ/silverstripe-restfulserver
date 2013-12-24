05c46a1d23b4e915009a8133ed057424dca35dcf
James Goodman <james.s.goodman@gmail.com>
2013-12-24 09:52:49 +0000

    Use DataFormatter to offer multiple response formats

--- code/RestfulServerV2.php
+++ code/RestfulServerV2.php
@@ -23,11 +23,34 @@ class RestfulServerV2 extends Controller {
 		$className = APIInfo::get_class_name_by_resource_name($resourceName);
 
 		if ($className === false) {
-			$this->httpError(404, 'Not found.'); // eventually needs to be displayed in requested format
+			return $this->httpError(404, 'Not found.'); // eventually needs to be displayed in requested format
 		}
 
-		// data here
-		return 'data here';
+		// only GET is supported for the time being so no checks are done
+		$formatter = $this->getDataFormatter();
+
+		if (is_null($formatter)) {
+			return $this->httpError(400, 'Invalid format type');
+		}
+
+		$this->getResponse()->addHeader('Content-Type', $formatter->getOutputContentType());
+
+		// very basic method for retrieving records for time being, improve this when adding sorting, pagination, etc.
+		$list = $className::get();
+
+		$formatter->setTotalSize($list->Count());
+		return $formatter->convertDataObjectSet($list);
+	}
+
+	private function getDataFormatter() {
+		// we only use the URL extension to determine format for the time being
+		$extension = $this->request->getExtension();
+
+		if (!$extension) {
+			$extension = self::$default_extension;
+		}
+
+		return DataFormatter::for_extension($extension);
 	}
 
 	public function showResource() {
