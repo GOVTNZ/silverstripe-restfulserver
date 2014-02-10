683bb9459b9293b6775770957996d0e6727d56a9
James Goodman <james.s.goodman@gmail.com>
2014-02-11 09:11:25 +1300

    Add test dataobject for relationship traversal tests

---
+++ tests/unit/StaffTestObject.php
@@ -0,0 +1,27 @@
+<?php
+
+class StaffTestObject extends DataObject implements TestOnly {
+
+	private static $db = array(
+		'Name' => 'Text',
+		'JobTitle' => 'Text'
+	);
+
+	private static $has_one = array(
+		'Manager' => 'StaffTestObject'
+	);
+
+	private static $has_many = array(
+		'DirectReports' => 'StaffTestObject'
+	);
+
+	private static $api_access = array(
+		'end_point_alias' => 'staff',
+		'singular_name' => 'staffMember',
+		'plural_name' => 'staff',
+		'relation_aliases' => array(
+			'DirectReports' => 'direct-reports'
+		)
+	);
+
+}
