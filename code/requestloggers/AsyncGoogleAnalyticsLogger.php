75b9d5ededf81e760e3f6ef4bd06248d0a318756
James Goodman <james.s.goodman@gmail.com>
2014-06-03 09:31:43 +1200

    Fix logging for URLs with query string

--- code/requestloggers/AsyncGoogleAnalyticsLogger.php
+++ code/requestloggers/AsyncGoogleAnalyticsLogger.php
@@ -5,10 +5,12 @@ namespace RestfulServer;
 class AsyncGoogleAnalyticsLogger implements RequestLogger {
 
 	public function log(\SS_HTTPRequest $request) {
-		$resourceName = escapeshellarg($request->param('ResourceName') . ' endpoint');
-		$url = escapeshellarg($request->getURL(true));
+		$resourceName = $request->param('ResourceName') . ' endpoint';
+		$url = $request->getURL(true);
 
-		exec('php cli-script.php dev/tasks/RestfulServer-SendAPIAnalytics dt=' . $resourceName . '\&dp=%2F' . $url . ' > /dev/null &');
+		$queryString = escapeshellarg('dt=' . $resourceName . '&dp=' . urlencode($url));
+
+		exec('php cli-script.php dev/tasks/RestfulServer-SendAPIAnalytics ' . $queryString . ' > /dev/null &');
 	}
 
 }
